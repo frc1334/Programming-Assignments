7002ef7d0ec7c48c98dbe598892f48936fcb7612
Megapead <gabrielciolac@gmail.com>
2014-05-14 21:11:14 -0400

    Added comments.
    To be honest these comments were just for my amusement. Mostly just the
    count comments.

--- gabriel Week 1.cpp
+++ gabriel Week 1.cpp
@@ -7,31 +7,33 @@ int sumSwap;
 
 int main()
 {
-    int num,i,count,n;
-    int r;
+    int num,i,count,n; //Declares num, i, count, n
+    int r; //does noting yet.
     cout << "Enter range: ";
     cin >> n;
     //cin >> r;
-    for(num = 1; num<=n; num++)
+    for(num = 1; num<=n; num++) //basic FOR loop that keeps going till user defined point.
     {
-        count = 0;
-        for(i=2; i<=num/2; i++)
+        count = 0; //the bats are equal to 0
+        for(i=2; i<=num/2; i++) //i starts off at 2 since 1 is arguably not a prime number, nothing past the half way mark can be divided. MORE LOOPS!!!
         {
-            if(num%i==0)
+            if(num%i==0) //if number diuvided by i=0 than it determins the number isn't prime.
             {
-                count++;
-                break;
+                count++;//"One, One bat HaHaHa"-The Count
+                break;//breaks loop
             }
         }
 
         if(count==0 && num!= 1)
         {
 
-            sumSwap=sum;
+            sumSwap=sum; //brings the sum to sumSwap. The sum gets nothing out of this exchange. :(
             sum=sumSwap+num;
         }
 
     }
-    cout<<sum;
+    cout<<sum;//displays sum
+
+    //MORE USELESS COMMENTS!!!!!!!
     return 0;
 }
