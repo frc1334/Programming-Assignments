ac629f3cd3aa7a7cc1364a1f399cfcbc237d2e2e
Megapead <gabrielciolac@gmail.com>
2014-05-26 15:27:56 -0400

    Now can input the first number
    ^^^^^^^^^^

--- gabriel Week 1.cpp
+++ gabriel Week 1.cpp
@@ -7,15 +7,15 @@ int sumSwap;
 
 int main()
 {
-    int num,i,count,n; //Declares num, i, count, n
+    int count,n; //Declares num, i, count, n
     int r; //does nothing yet.
     cout << "Enter range: ";
     cin >> n;
-    //cin >> r;
-    for(num = 1; num<=n; num++) //basic FOR loop that keeps going till user defined point.
+    cin >> r;
+    for(int num = n; num<=r; num++) //basic FOR loop that keeps going till user defined point.
     {
         count = 0; //the bats are equal to 0
-        for(i=2; i<=num/2; i++) //i starts off at 2 since 1 is arguably not a prime number, nothing past the half way mark can be divided. MORE LOOPS!!!
+        for(int i=2; i<=num/2; i++) //i starts off at 2 since 1 is arguably not a prime number, nothing past the half way mark can be divided. MORE LOOPS!!!
         {
             if(num%i==0) //if number diuvided by i=0 than it determins the number isn't prime.
             {
