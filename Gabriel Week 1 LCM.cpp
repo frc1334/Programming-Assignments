1d534112189f03521e5f330eb73c4a93179ff4c2
Megapead <gabrielciolac@gmail.com>
2014-06-01 17:19:17 -0400

    LCM program changed

--- Gabriel Week 1 LCM.cpp
+++ Gabriel Week 1 LCM.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
+
+
+int numberOne;
+int numberTwo;
+int GreatestCommonDivisor; //Declaring all the important bits
+int LowestCommonMultiple;
+
 using namespace std;
+
 
 int main()
 {
 
-    int numberOne=0; //Bunch of declaring.
-    int numberTwo=0;
-    int lowestCommonMultiple;
-    int multiple=numberTwo;//set multiple=numberTwo just so I can place it in the loop.
-
-    cout<<"Enter Your First Number: ";
-    cin>>numberOne;                        //User input of values
-    cout<<"Enter Your Second Number: ";
-    cin>>numberTwo;
-
-    for(int i=1; i<100000; i++) //Loop start, I just put some absurdly large number in since, the loop autobreaks.
+    cout << "Enter the lowest of the two numbers: ";
+    cin >> numberOne;
+    cout << "Enter the largest of the two numbers: "; //Getting the values of numberOne and numberTwo
+    cin >> numberTwo;
+    for (int i=1;i<numberTwo; i++) //for loop
     {
-        multiple=numberTwo*i;//numberTwo is the only multiplying valuel. Multiplies by number in the loop
-
-        if(multiple%numberOne==0) //If multiple divided by numberOne=0 than lowestCommonMultiple must be equal to the current multiple. Breaks loop.
+        if(numberOne%i==0 && numberTwo%i==0) //if both numbers divided by the value of i are equal to 0 than that's the greatest commond divisor
         {
-            lowestCommonMultiple=multiple;
-            break;
+            GreatestCommonDivisor=i;
         }
 
     }
-    cout << "The lowest common multiple is "<<lowestCommonMultiple<< endl;
+
+
+    LowestCommonMultiple =numberOne*numberTwo/GreatestCommonDivisor;
+
+    cout << "The lowest common multiple of "<<numberOne<<" and "<<numberTwo<<" is " << LowestCommonMultiple; 
+
+
     return 0;
 }
